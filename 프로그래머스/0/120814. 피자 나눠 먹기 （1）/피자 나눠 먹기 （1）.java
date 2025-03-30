54d4137e2abf2741c926beb13baa7228eac49b42
똥 <choscdd@gmail.com>
2025-03-30 13:58:19 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int n) {
-        if (n%7 == 0 ) return n/7;
-        else return n/7 + 1;
+        return (n%7==0) ? n/7 : n/7 + 1;
             
     }
 }
