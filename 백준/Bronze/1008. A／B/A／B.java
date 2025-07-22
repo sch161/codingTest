78602e6f5147e8b51b2ab51f8b9eff3f9fea6a89
똥 <choscdd@gmail.com>
2025-07-22 17:50:45 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 18004 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Double A = scan.nextDouble();
+        Double B = scan.nextDouble();
+        System.out.println(A / B);
+    }
+}
