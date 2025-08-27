e57250ed2e81b4f6f270443c0aae7c016e02dc26
똥 <choscdd@gmail.com>
2025-08-27 13:29:55 +0900

    [Bronze V] Title: 시험 성적, Time: 176 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        if(x >= 90 || x >= 100)
+            System.out.println("A");
+        else if(x >= 80 || x > 90)
+            System.out.println("B");
+        else if(x >= 70 || x > 80)
+            System.out.println("C");
+        else if(x >= 60 || x > 70)
+            System.out.println("D");
+        else System.out.println("F");
+    }
+}
