e8a9f10ca83ad01583aeae4cf74dece8368224f7
똥 <choscdd@gmail.com>
2025-09-23 13:34:49 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,10 +2,7 @@ class Solution {
     public int solution(int n) {
         String x = Integer.toString(n);
         String arr[] = x.split("");
-        int num[] = new int[arr.length];
-        for(int i = 0; i < num.length; i++) {
-            num[i] = Integer.parseInt(arr[i]);
-        }
+        int num[] = Integer.parseInt(arr);
         int result = 0;
         for(int i = 0; i < arr.length; i++) {
             result += num[i];
