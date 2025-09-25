a8b353fda7d4aa51d988fa1ac00199c34a2dc1ba
똥 <choscdd@gmail.com>
2025-09-25 13:44:55 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.58 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int num1 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        int num2 = numbers[0] * numbers[1];
+        
+        return (num1 > num2) ? num1 : num2;
+    }
+}
