82ba35953f8509850ae47b659bc27006e7574995
똥 <choscdd@gmail.com>
2025-12-03 10:46:18 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int result = 0;
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] > 0) {
+                result = -1;
+            } else {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
