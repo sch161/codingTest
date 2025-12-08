0be6d81d32174c0e2ad583d649fc7d80990221b3
똥 <choscdd@gmail.com>
2025-12-08 14:51:30 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 1.24 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int result[] = new int[5];
+        for(int i = 0; i < 5; i++) {
+            result[i] = num_list[i];
+        }
+        return result;
+    }
+}
