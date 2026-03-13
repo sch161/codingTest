2c7d5df7ed2608e6707ae1286b9a540cdb8a5b39
똥 <choscdd@gmail.com>
2026-03-13 10:06:57 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.74 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String result = "";
+        String arr[] = s.split("");
+        int n = 0;
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < arr.length; i++) {
+            n = 0;
+            for(int j = 0; j < arr.length; j++) {
+                if(arr[i].equals(arr[j])) {
+                    n++;
+                }
+            }
+            if(n == 1) {
+                result += arr[i];
+            }
+        }
+        return result;
+    }
+}
