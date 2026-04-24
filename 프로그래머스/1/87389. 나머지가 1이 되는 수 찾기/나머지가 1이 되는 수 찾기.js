49cb12cc78c08b55ea19c795ff234665f2dc7ea1
똥 <choscdd@gmail.com>
2026-04-24 12:04:30 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 6.48 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = n;
+    for(let i = 0; i < n; i++) {
+        if(n % i === 1) {
+            if(answer > i) {
+                answer = i;
+            }
+        }
+    }
+    return answer;
+}
